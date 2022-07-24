74cb23a7de35150f442bae0f886ead8fcbd41dc1
Pengfei Gao <gpfdeveloper@gmail.com>
2022-07-24 09:42:34 +0800

    history item enhancement

--- src/components/project/issue/activity/ActivityHistory.tsx
+++ src/components/project/issue/activity/ActivityHistory.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useState } from 'react';
+import { FunctionComponent } from 'react';
 import { Box } from '@mui/material';
 import { sampleHistory } from 'dummyData/dummyData';
 import { useProject } from 'contexts/project-context';
@@ -6,13 +6,12 @@ import ActivityHistoryItem from './ActivityHistoryItem';
 
 const ActivityHistory: FunctionComponent = () => {
   const { currentIssue } = useProject();
-  const initHistory = sampleHistory
+  const history = sampleHistory
     .filter((item) => item.issueId === currentIssue!.id)
     .sort(
       (a, b) =>
         new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
     );
-  const [history, setHistory] = useState(initHistory);
   return (
     <Box sx={{ mt: 2 }}>
       {history.map((item) => (
--- src/components/project/issue/activity/ActivityHistoryItem.tsx
+++ src/components/project/issue/activity/ActivityHistoryItem.tsx
@@ -1,9 +1,127 @@
 import { FunctionComponent } from 'react';
-import { Box, Typography } from '@mui/material';
-import type { History } from 'types/types';
+import { Box, Chip, Typography } from '@mui/material';
+import type {
+  History,
+  People,
+  ProjectIssuePriority,
+  ProjectIssueStatus,
+} from 'types/types';
 import moment from 'moment';
 import StringAvatar from 'components/shared/StringAvatar';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import DragHandleIcon from '@mui/icons-material/DragHandle';
+
+const StatusHistory: FunctionComponent<{
+  from: ProjectIssueStatus;
+  to: ProjectIssueStatus;
+}> = ({ from, to }) => {
+  const getColor = (status: ProjectIssueStatus) => {
+    let color: 'default' | 'primary' | 'success' = 'default';
+    if (status === 'IN PROGRESS') color = 'primary';
+    if (status === 'DONE') color = 'success';
+    return color;
+  };
+  return (
+    <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+      <Chip size="small" color={getColor(from)} label={from} />
+      <ArrowForwardIcon />
+      <Chip size="small" label={to} color={getColor(to)} />
+    </Box>
+  );
+};
+
+const PriorityHistory: FunctionComponent<{
+  from: ProjectIssuePriority;
+  to: ProjectIssuePriority;
+}> = ({ from, to }) => {
+  const getIcon = (priority: ProjectIssuePriority) => {
+    let icon = <DragHandleIcon color="warning" />;
+    switch (priority) {
+      case 'Highest':
+        icon = <KeyboardDoubleArrowUpIcon color="error" />;
+        break;
+      case 'High':
+        icon = <KeyboardArrowUpIcon color="error" />;
+        break;
+      case 'Low':
+        icon = <KeyboardArrowDownIcon color="info" />;
+        break;
+      case 'Lowest':
+        icon = <KeyboardDoubleArrowDownIcon color="info" />;
+        break;
+    }
+    return icon;
+  };
+  const iconFrom = getIcon(from);
+  const iconTo = getIcon(to);
+  return (
+    <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.4 }}>
+        {iconFrom}
+        <Typography variant="body2" fontWeight={500}>
+          {from}
+        </Typography>
+      </Box>
+      <ArrowForwardIcon />
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.4 }}>
+        {iconTo}
+        <Typography variant="body2" fontWeight={500}>
+          {to}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+const PeopleHistory: FunctionComponent<{ from: People; to: People }> = ({
+  from,
+  to,
+}) => {
+  return (
+    <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.4 }}>
+        <StringAvatar height={30} width={30} name={from.name} />
+        <Typography variant="body2" fontWeight={500}>
+          {from.name}
+        </Typography>
+      </Box>
+      <ArrowForwardIcon />
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.4 }}>
+        <StringAvatar height={30} width={30} name={to.name} />
+        <Typography variant="body2" fontWeight={500}>
+          {to.name}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+const DueHistory: FunctionComponent<{
+  from: string | undefined;
+  to: string | undefined;
+}> = ({ from, to }) => {
+  const _from = from ? moment(from).format('LL') : 'None';
+  const _to = to ? moment(to).format('LL') : 'None';
+  const getColor = (due: string) => {
+    if (due === 'None') return 'text.disabled';
+    return 'text.primary';
+  };
+  return (
+    <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+      <Typography variant="body2" fontWeight={500} color={getColor(_from)}>
+        {_from}
+      </Typography>
+      <ArrowForwardIcon />
+      <Typography variant="body2" fontWeight={500} color={getColor(_to)}>
+        {_to}
+      </Typography>
+    </Box>
+  );
+};
 
 type Props = {
   item: History;
@@ -16,6 +134,42 @@ const ActivityHistoryItem: FunctionComponent<Props> = ({ item }) => {
   }
   if (item.field === 'Comment') {
     action = 'deleted a';
+  }
+  let content;
+  if (item.field === 'Status') {
+    content = (
+      <StatusHistory
+        from={item.from as ProjectIssueStatus}
+        to={item.to as ProjectIssueStatus}
+      />
+    );
+  }
+  switch (item.field) {
+    case 'Status':
+      content = (
+        <StatusHistory
+          from={item.from as ProjectIssueStatus}
+          to={item.to as ProjectIssueStatus}
+        />
+      );
+      break;
+    case 'Due':
+      content = <DueHistory from={item.from} to={item.to} />;
+      break;
+    case 'Priority':
+      content = (
+        <PriorityHistory
+          from={item.from as ProjectIssuePriority}
+          to={item.to as ProjectIssuePriority}
+        />
+      );
+      break;
+    case 'Assignee':
+    case 'Reporter':
+      const pFrom = JSON.parse(item.from!) as People;
+      const pTo = JSON.parse(item.to!) as People;
+      content = <PeopleHistory from={pFrom} to={pTo} />;
+      break;
   }
   return (
     <Box sx={{ display: 'flex', gap: 2, my: 4 }}>
@@ -33,13 +187,7 @@ const ActivityHistoryItem: FunctionComponent<Props> = ({ item }) => {
             {moment(item.createdAt).format('LLL')}
           </Typography>
         </Box>
-        {item.from && item.to && (
-          <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
-            <Typography variant="body2">{item.from}</Typography>
-            <ArrowForwardIcon />
-            <Typography variant="body2">{item.to}</Typography>
-          </Box>
-        )}
+        {content}
       </Box>
     </Box>
   );
