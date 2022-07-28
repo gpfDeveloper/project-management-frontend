4f5f918cd749b64d389ab1b1f4784a5c61e103be
Pengfei Gao <gpfdeveloper@gmail.com>
2022-07-28 11:12:24 +0800

    bug fix - set current project when loading issue detail page

--- src/pages/IssueDetailPage.tsx
+++ src/pages/IssueDetailPage.tsx
@@ -3,7 +3,7 @@ import { ProjectLayout } from 'components/layout/ProjectLayout';
 import IssueDetailWrapper from 'components/project/issue/IssueDetailWrapper';
 import { FunctionComponent, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { getIssue } from 'dummyData/dummyData';
+import { getIssue, getProject } from 'dummyData/dummyData';
 import { useProject } from 'contexts/project-context';
 
 const IssueDetailPage: FunctionComponent = () => {
@@ -11,15 +11,17 @@ const IssueDetailPage: FunctionComponent = () => {
   // pathname: /projects/project1/issues/773096cf-9680-4a2a-9f4b-28e1db909392
   const paths = location.pathname.split('/');
   const issueId = paths[4];
-  const { setCurrentIssue } = useProject();
+  const { setCurrentIssue, setCurrentProject } = useProject();
 
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const issue = getIssue(issueId);
+    const project = getProject(issue!.projectId);
     setCurrentIssue(issue);
+    setCurrentProject(project);
     setLoading(false);
-  }, [issueId, setCurrentIssue]);
+  }, [issueId, setCurrentIssue, setCurrentProject]);
 
   return (
     <ProjectLayout>
