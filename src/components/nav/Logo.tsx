3d47c7967ae2aa41e5d5002dc1b6f2262a770ca4
Pengfei Gao <gpfdeveloper@gmail.com>
2022-08-22 09:15:51 +0800

    update font since google font not available from time to time

--- src/components/nav/Logo.tsx
+++ src/components/nav/Logo.tsx
@@ -4,24 +4,23 @@ import { Box, Typography } from '@mui/material';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 
 const Logo: FunctionComponent = () => {
-  return (
-    <Link to="/">
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <DashboardIcon color="primary" fontSize="large" />
-        <Typography
-          variant="h5"
-          noWrap
-          sx={{
-            ml: 0.6,
-            fontFamily: 'Roboto Condensed',
-            color: 'text.primary',
-          }}
-        >
-          Project Management
-        </Typography>
-      </Box>
-    </Link>
-  );
+    return (
+        <Link to="/">
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <DashboardIcon color="primary" fontSize="large" />
+                <Typography
+                    variant="h6"
+                    noWrap
+                    sx={{
+                        ml: 0.6,
+                        color: 'text.primary',
+                    }}
+                >
+                    Project Management
+                </Typography>
+            </Box>
+        </Link>
+    );
 };
 
 export default Logo;
